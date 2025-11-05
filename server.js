4fdfbf0081f4d09f5dbac938fffa044494cd32db
Keirin Obando <kobando@coopeguanacaste.com>
2025-11-05 03:32:58 -0600

    ci: health + nuclei en development

--- server.js
+++ server.js
@@ -59,3 +59,42 @@ app.get('/health', (req, res) => res.send('ok'));
 app.get('/health', (req, res) => {
   res.status(200).send('ok');
 });
+
+const promClient = require('prom-client');      
+promClient.collectDefaultMetrics();              
+const register = promClient.register;             
+
+const httpRequestDuration = new promClient.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duración de requests HTTP',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [0.05, 0.1, 0.3, 0.6, 1, 2, 5]
+});
+
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+  res.on('finish', () => {
+    end({
+      method: req.method,
+      route: req.route?.path || req.path,  
+      status_code: res.statusCode
+    });
+  });
+  next();
+});
+
+// Exponer /metrics
+app.get('/metrics', async (_req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
+// --- HEALTHCHECK ---
+app.get('/healthz', (req, res) => {
+  res.status(200).json({ ok: true });
+});
+
+app.get('/security/canary', (_req, res) => {
+  res.set('X-NUCLEI-CANARY', 'true');
+  res.status(200).send('NUCLEI_CANARY_OK');
+});
