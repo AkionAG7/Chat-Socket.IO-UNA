4a47224a5f7c39884e91b16fc50104417a4a2b60
Akion Cheng <akion.cheng.jimenez@est.una.ac.cr>
2025-11-05 14:34:36 -0600

    cambios key

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/home', (_,res)=>res.sendFile(path.join(__dirname,'public','index.html')));
-app.get('/chat', (_,res)=>res.sendFile(path.join(__dirname,'public','chat.html')));
-app.get('/calculadora', (_,res)=>res.sendFile(path.join(__dirname,'public','calculadora.html')));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/home", (_, res) =>
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+);
+app.get("/chat", (_, res) =>
+  res.sendFile(path.join(__dirname, "public", "chat.html"))
+);
+app.get("/calculadora", (_, res) =>
+  res.sendFile(path.join(__dirname, "public", "calculadora.html"))
+);
 
 module.exports = app;
